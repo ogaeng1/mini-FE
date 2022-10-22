4d9498f93253cb70f55d3bbc7716ae1b0368bf21
sunguk-kim <ksu930310@gmail.com>
2022-10-22 10:46:19 +0900

    "react-cookie"패키지 설치, index에 "CookieProvider" 기입

--- src/redux/modules/userSlice.js
+++ src/redux/modules/userSlice.js
@@ -1,57 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-
-export const createUser = (data) => {
-  return async function (dispatch) {
-    // console.log(data);
-    await axios.post("members/signup", data, {
-        "Content-Type": "application/json",
-        withCredentials: true,
-      })
-      .then((response) => {
-        // console.log(response);
-        if (response.data.success === false) {
-          return window.alert(response.data.err.msg);
-        } else {
-          return window.alert(`${response.data.data.nickname}님 회원가입을 축하드립니다!`), window.location.replace("/login");
-        }
-      })
-      .catch((error) => {
-        if (error.response.status === 400) {
-          alert(error.response.data.message);
-        }
-      });
-  };
-};
-
-export const loginUser = (data) => {
-  return async function (dispatch) {
-    await axios
-      .post("members/login", data, {
-        "Content-Type": "application/json",
-        withCredentials: true,
-      })
-      .then((response) => {
-        console.log(response);
-        if (response.data.success === true) {
-          return (
-            sessionStorage.setItem("token", response.headers.authorization),
-            // cookies.save("refresh-token", response.headers["refresh-token"]),
-            sessionStorage.setItem("nickname", response.data.data.nickname),
-            alert(`${sessionStorage.nickname}님 환영합니다.`),
-            window.location.replace("/")
-          );
-        } else {
-          return window.alert(response.data.error.msg);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        
-      });
-  };
-};
-
+import { setCookie } from "../../components/global/cookie";
 
 export const __validateEmail = createAsyncThunk(
     "VALIDATE_EMAIL",
@@ -84,7 +33,20 @@ export const __addUser = createAsyncThunk(
             axios.post("http://localhost:3001/signup", arg);
             return thunkAPI.fulfillWithValue(arg);
         } catch(e){
-        return thunkAPI.rejectWithValue(e)
+            return thunkAPI.rejectWithValue(e)
+        }
+    }
+)  
+
+export const __loginUser = createAsyncThunk(
+    "LOGIN_USER",
+    async(arg, thunkAPI) => {
+        try{
+            await axios.post("http://localhost:3001/login", arg)
+            .then((res)=> setCookie('email', res.check.data.AccessToken));
+            return thunkAPI.fulfillWithValue(arg);
+        } catch(e){
+            return thunkAPI.rejectWithValue(e)
         }
     }
 )  
@@ -148,6 +110,18 @@ const userSlice = createSlice({
         alert(`${action.paylod.name}님 회원가입을 축하합니다.`)
     },
     [__addUser.rejected]: (state, action) => {
+        state.isLoading = false; 
+        state.message = "데이터를 불러오지 못했습니다.";
+    },
+
+    [__loginUser.pending]: (state, action) => {
+        state.isLoading = true; 
+    },
+    [__loginUser.fulfilled]: (state, action) => {
+        state.isLoading = false;
+        alert(`${action.paylod.email}님 환영합니다.`)
+    },
+    [__loginUser.rejected]: (state, action) => {
         state.isLoading = false; 
         state.message = "데이터를 불러오지 못했습니다.";
     },
